60e7ec81a0b43e1e36429109d05e681e411d18e6
hzrd149 <github@hzrd149.com>
2025-05-16 20:44:04 -0500

    Fix `CommentsQuery` not returning comments for older versions of replaceable events

--- packages/core/src/queries/comments.ts
+++ packages/core/src/queries/comments.ts
@@ -1,14 +1,14 @@
 import { Filter, NostrEvent } from "nostr-tools";
-import { Query } from "../query-store/index.js";
-import { COMMENT_KIND, getEventUID } from "../helpers/index.js";
 import { isAddressableKind } from "nostr-tools/kinds";
+import { COMMENT_KIND, getReplaceableAddress } from "../helpers/index.js";
+import { Query } from "../query-store/index.js";
 
 /** Returns all NIP-22 comment replies for the event */
 export function CommentsQuery(parent: NostrEvent): Query<NostrEvent[]> {
   return (events) => {
-    const filter: Filter = { kinds: [COMMENT_KIND], "#e": [parent.id] };
-    if (isAddressableKind(parent.kind)) filter["#a"] = [getEventUID(parent)];
+    const filters: Filter[] = [{ kinds: [COMMENT_KIND], "#e": [parent.id] }];
+    if (isAddressableKind(parent.kind)) filters.push({ kinds: [COMMENT_KIND], "#a": [getReplaceableAddress(parent)] });
 
-    return events.timeline(filter);
+    return events.timeline(filters);
   };
 }
