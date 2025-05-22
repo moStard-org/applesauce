7635526aee2278c7fff18cda72de8870213c8a76
hzrd149 <github@hzrd149.com>
2025-05-22 15:00:17 -0500

    fix build

--- packages/examples/src/examples/bunker-login.tsx
+++ packages/examples/src/examples/bunker-login.tsx
@@ -1,9 +1,7 @@
-import { useObservable } from "applesauce-react/hooks";
+import { EventFactory } from "applesauce-factory";
 import { RelayPool } from "applesauce-relay";
 import { NostrConnectSigner } from "applesauce-signers";
 import { useState } from "react";
-import { BehaviorSubject } from "rxjs";
-import { EventFactory } from "applesauce-factory";
 import JsonBlock from "../components/json-block";
 
 // Create a relay pool to make relay connections
